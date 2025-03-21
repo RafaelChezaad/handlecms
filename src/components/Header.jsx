deb3d751f4ec8b543904a637b28b7fffc1b22e95
Rafael <sotomedinayunior@gmail.com>
2025-03-21 17:39:59 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
             src={Logo}
             alt="Teamelizabeth logo"
             width={200}
-            height={100}
+            height={80}
             title="Teamelizabeth"
             loading="lazy"
             className={styles.logo}
